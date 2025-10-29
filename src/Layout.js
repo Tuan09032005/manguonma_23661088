be05686b7586b75996b69a0f71ccda7a3b4f2fca
PTuan <162488223+Tuan09032005@users.noreply.github.com>
2025-10-29 10:58:35 +0000

    chinh sua lai code

--- src/Layout.js
+++ src/Layout.js
@@ -44,7 +44,92 @@ const Layout = () => {
             </div>
             <div>Tim kiem</div>
           </div>
-          <div id="menubar" className="menubar"></div>
+          <div id="menubar" className="menubar">
+            <nav class="navbar">
+              {" "}
+              <ul>
+                <li>
+                  <a href="#">GIỚI THIỆU</a>
+                  <ul>
+                    <li>
+                      <a href="#">lịch sử hình thành và phát triển</a>
+                    </li>
+                    <li>
+                      <a href="#">bộ máy tổ chức</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">TIN TỨC</a>
+                </li>
+                <li>
+                  <a href="#">TUYỂN SINH</a>
+                  <ul>
+                    <li>
+                      <a href="#">tuyển sinh 2025</a>
+                    </li>
+                    <li>
+                      <a href="#">tuyển sinh chính quy</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">CÔNG KHAI GIÁO DỤC</a>
+                  <ul>
+                    <li>
+                      <a href="#">công khai thường niên</a>
+                    </li>
+                    <li>
+                      <a href="#">chất lượng đào tạo</a>
+                    </li>
+                    <li>
+                      <a href="#">chuẩn đầu ra</a>
+                    </li>
+                    <li>
+                      <a href="#">đội ngũ giảng viên</a>
+                    </li>
+                    <li>
+                      <a href="#">cơ sở vật chất</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">CƠ CẤU TỔ CHỨC</a>
+                  <ul>
+                    <li>
+                      <a href="#">đảng ,toàn thể</a>
+                    </li>
+                    <li>
+                      <a href="#">ban giám hiệu</a>
+                    </li>
+                    <li>
+                      <a href="#">phòng ban</a>
+                    </li>
+                    <li>
+                      <a href="#">khoa chuyên môn</a>
+                    </li>
+                    <li>
+                      <a href="#">trung tâm</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a href="#">LIÊN KẾT</a>
+                  <ul>
+                    <li>
+                      <a href="#">thông tin từ sở dt tphcm</a>
+                    </li>
+                    <li>
+                      <a href="#">cổng dky tuyển sinh</a>
+                    </li>
+                    <li>
+                      <a href="#">sổ tay sinh viên</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </header>
       <body>
